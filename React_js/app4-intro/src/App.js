7f8fe00ba96d3dbb27a26e610ca7e01dd2c23fc0
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-03-04 16:24:19 +0530

    React

--- React_js/app4-intro/src/App.js
+++ React_js/app4-intro/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Yemmu } from './Yemmu';
 
 function App() {
   return (
     <div className="App">
       <h1>yamuna</h1>
       <p>react  js introduction</p>
+      <Yemmu/>
     </div>
   );
 }

c8930ecabbd211d8c374aee2a6494c6704009d0c
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-03-12 11:04:57 +0530

    react

--- React_js/app10-intro/src/App.js
+++ React_js/app10-intro/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './Components/User';
+import Users from './Components/Users';
 
 function App() {
   return (
     <div className="App">
-      <User/>
+      {/* <User/> */}
+      <Users/>
       
     </div>
   );

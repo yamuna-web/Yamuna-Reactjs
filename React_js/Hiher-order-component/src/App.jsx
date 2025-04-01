4064d584c224523051f7bb696b87e757f108628f
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-04-01 12:26:36 +0530

    react

--- React_js/Hiher-order-component/src/App.jsx
+++ React_js/Hiher-order-component/src/App.jsx
@@ -3,19 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ClickComponent from './Component/ClickComponent'
-import HoverComponent from './Component/HoverComponent'
-import Click from './components/Click'
-import Hovercom from './components/Hovercom'
+
+import Clickcom from './MainComponent/Clickcom'
+import Hovercompo from './MainComponent/Hovercompo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <ClickComponent/>
-      <HoverComponent/>
+  {/* <ClickComponent/>
+      <HoverComponent/> */}
       {/* <Click/> */}
       {/* <Hovercom/> */}
+      <Clickcom/>
+      <Hovercompo/>
     </>
   )
 }

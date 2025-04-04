2caf142c49418b6eeee341f847f8049884549e28
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-04-04 12:14:04 +0530

    reactjs

---
+++ React_js/spa-project/src/UserDetails.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserDetails extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to User Details Page !!</h2>
+      </div>
+    )
+  }
+}

53f013d1d45613e669fe2a6360ba64cfcd99c03c
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-04-11 21:44:59 +0530

    react

---
+++ React_js/app13-react-render-props/src/Mouseover.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export default  class Mouseovr extends Component{
+    render(){
+        return(<div>
+           
+            <h1>{this.props.count}</h1>
+            <button onMouseOver={this.props.increment}>increment</button>
+            <button onMouseOver={this.props.decrement}>decrement</button>
+            <button onMouseOver={this.props.reset}>reset</button>
+        </div>)
+    }
+}

22f31d8da7e9cafb7225342e1623a9687ea4658a
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-05-18 16:20:04 +0530

    React

---
+++ React_js/app5-intro/src/User.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class User extends Component{
+    constructor(){
+        
+    }
+    render(){
+        return <div>
+
+        </div>
+    }
+}

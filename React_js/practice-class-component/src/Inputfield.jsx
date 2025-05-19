2c87f363ce37666a084921fe266b4fe8f60092a6
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-05-19 21:48:59 +0530

    class and functional component

---
+++ React_js/practice-class-component/src/Inputfield.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+
+export default class Inputfield extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+          placeholder=""
+        />
+        <p>inputvalue: {this.state.inputValue}</p>
+      </div>
+    );
+  }
+}

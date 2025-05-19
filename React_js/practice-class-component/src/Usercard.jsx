b3a737f18d4d65f96fed1624833480ad871d912c
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-05-19 21:48:39 +0530

    class and functional component

---
+++ React_js/practice-class-component/src/Usercard.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+export default class UserCard extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return (<>
+            <h1>{this.props.name}</h1>
+            <h1>role:{this.props.role}</h1>
+        </>)
+    }
+}
+UserCard.defaultProps = {
+    role: 'Guest'
+};

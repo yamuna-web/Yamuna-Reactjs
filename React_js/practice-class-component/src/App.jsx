51e4570bd82fb614484e8603077afa2a35c9aad9
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-05-22 21:14:41 +0530

    class and functional

--- React_js/practice-class-component/src/App.jsx
+++ React_js/practice-class-component/src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Yamuna from './Class_component_creation/Yamuna'
 import Counter from './Count/Counter'
-import Parent from './Components_with_props/Parent'
-
 import UserCard from './Usercard'
 import Timer from './Timer'
 import Loginstatus from './Loginstatus'
@@ -13,6 +11,9 @@ import Toggle from './Toggle'
 import Inputfield from './Inputfield'
 import Maxdistance from './Maxdistance'
 import Greeting from './Component_with_default_props/Greeting'
+import Parent1 from './Components_with_props/Parent1'
+import Greeting1 from './Component_with_default_props/Greetprops1'
+import Parent from './Parent'
 
 
 
@@ -23,16 +24,17 @@ function App() {
    <>
   {/* <Yamuna/> */}
 {/* <Counter/> */}
-<Parent/>
 {/* <Greeting/> */}
 {/* <UserCard/> */}
 {/* <Timer/> */}
 {/* <Loginstatus/> */}
 {/* <Parent/> */}
 {/* <Toggle/> */}
-{/* <Inputfield/> */}
+<Inputfield/>
 {/* <Loginstatus/> */}
 {/* <Maxdistance/> */}
+{/* <Parent1/> */}
+{/* <Greeting1/> */}
    </>
   )
 }

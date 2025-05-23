4126a361f3e2108596b5d139bc4ab6d4c7673324
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-05-23 21:30:37 +0530

    class component

--- React_js/practice-class-component/src/App.jsx
+++ React_js/practice-class-component/src/App.jsx
@@ -14,6 +14,18 @@ import Greeting from './Component_with_default_props/Greeting'
 import Parent1 from './Components_with_props/Parent1'
 import Greeting1 from './Component_with_default_props/Greetprops1'
 import Parent from './Parent'
+import Question1 from './Question1'
+import Question2 from './Question2'
+import Question3 from './Question3'
+import Question4 from './Question4'
+import Question5 from './Question5'
+import Question6 from './Question6'
+import Question7 from './Question7'
+import Practice1 from './Practice1'
+import Practice2 from './Practice2'
+import Practice3 from './Practice3'
+import Practice4 from './Practice4'
+import Practice5 from './Practice5'
 
 
 
@@ -30,11 +42,23 @@ function App() {
 {/* <Loginstatus/> */}
 {/* <Parent/> */}
 {/* <Toggle/> */}
-<Inputfield/>
+{/* <Inputfield/> */}
 {/* <Loginstatus/> */}
 {/* <Maxdistance/> */}
 {/* <Parent1/> */}
 {/* <Greeting1/> */}
+{/* <Question1/> */}
+{/* <Question2/> */}
+{/* <Question3/> */}
+{/* <Question4/> */}
+{/* <Question5/> */}
+{/* <Question6/> */}
+{/* <Question7/> */}
+{/* <Practice1/> */}
+{/* <Practice2/> */}
+{/* <Practice3/> */}
+{/* <Practice4/> */}
+<Practice5/>
    </>
   )
 }

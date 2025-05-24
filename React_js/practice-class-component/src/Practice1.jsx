69eb6420eab942f64ade3de7d3e2703510e0002e
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-05-24 21:37:50 +0530

    class component

--- React_js/practice-class-component/src/Practice1.jsx
+++ React_js/practice-class-component/src/Practice1.jsx
@@ -1,8 +1,8 @@
 import { Component } from "react";
 
 export default class Practice1 extends Component{
-    constructor(props){
-        super(props)
+    constructor(){
+        super()
         this.state={
             count:0
         }

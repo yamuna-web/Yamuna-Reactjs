224b139d2c1cf82274166eb1e955a40015ee502f
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-05-25 21:42:31 +0530

    class

---
+++ React_js/practice-class-component/src/Toggle3.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+export default class Toggle3 extends Component{
+    constructor(){
+        super()
+        this.state={
+            ison:true
+        }
+    }
+    handletoggle=()=>{
+        this.setState({
+            ison:!this.state.ison
+        })
+    }
+    render(){
+        return <div>
+           <h1>{this.state.ison?"on":"off"}</h1>
+           <button onClick={this.handletoggle}>click here</button>
+        </div>
+    }
+}

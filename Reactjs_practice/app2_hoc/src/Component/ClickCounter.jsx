4285776e9aae4344db76e66643651630b4fe9af2
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-06-19 21:22:31 +0530

    Components

---
+++ Reactjs_practice/app2_hoc/src/Component/ClickCounter.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import Hoc from "./Hoc";
+
+class ClickCounter extends Component{
+render(){
+    const {count,increment,decrement,reset}=this.props
+    return <div>
+        <h2>count:{count}</h2>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
+        <button onClick={reset}>reset</button>
+    </div>
+}
+
+}
+export default Hoc(ClickCounter)

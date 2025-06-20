90f53d3b0f84b42d6c2c22f2791e1bbcf1bc9017
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-06-20 16:04:41 +0530

    Practice

--- Reactjs_practice/app1_render_props/src/App.jsx
+++ Reactjs_practice/app1_render_props/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CommonCounter from './Render_props/CommonCounter'
+import Common from './Render/Common'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <CommonCounter/>
+    {/* <CommonCounter/> */}
+    <Common/>
     </>
   )
 }

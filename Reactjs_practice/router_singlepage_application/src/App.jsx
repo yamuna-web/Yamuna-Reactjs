54d2fb879d79ec088ec0086cfef5b7beea6d1068
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-06-23 16:13:41 +0530

    Component

--- Reactjs_practice/router_singlepage_application/src/App.jsx
+++ Reactjs_practice/router_singlepage_application/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const Register = lazy(() => import("./Components/Register"));
+const Login = lazy(() => import("./Components/Login"));
+const Dashboard = lazy(() => import("./Components/Dashboard"));
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

da3622fc0d19f84b46ee183b4003aa399a33134e
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-06-23 16:14:02 +0530

    Component

---
+++ Reactjs_practice/router_singlepage_application/src/Components/Login.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [erroMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const newUser = { ...user };
+    newUser[e.target.name] = e.target.value;
+    setUser(newUser);
+  };
+  const handleSubmit = () => {
+    console.log(user);
+    const allUsersFromLocalStorage = JSON.parse(localStorage.getItem("users"));
+    console.log(allUsersFromLocalStorage);
+    var isLoginSuccss = false;
+    allUsersFromLocalStorage.forEach((usr) => {
+      console.log(usr);
+      if (usr.email === user.email && usr.password === user.password) {
+        isLoginSuccss = true;
+      }
+    });
+
+    if (isLoginSuccss) {
+      console.log("Login Successfull !!");
+      navigate("/dashboard");
+    } else {
+      setErrorMessage("Login Failed !! .Please use Valid Credentials .");
+      console.log("Login Failed !!");
+    }
+  };
+  return (
+    <div>
+      <h2>Welcome to Login page</h2>
+
+      <form>
+        <label htmlFor="">Email :</label>
+        <input
+          type="text"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <label htmlFor="">Password :</label>
+        <input
+          type="text"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <button type="button" onClick={handleSubmit}>
+          Register
+        </button>
+      </form>
+      <p style={{ color: "red" }}>{erroMessage}</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ Reactjs_practice/router_singlepage_application/src/Components/Register.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [users, setUsers] = useState([]);
+  const handleChange = (e) => {
+    const newUser = { ...user };
+    newUser[e.target.name] = e.target.value;
+    setUser(newUser);
+  };
+
+  const handleSubmit = () => {
+    const newUsers = JSON.parse(localStorage.getItem("users"));
+    if (newUsers !== null) {
+      newUsers.push(user);
+      localStorage.setItem("users", JSON.stringify(newUsers));
+    } else {
+      localStorage.setItem("users", JSON.stringify([user]));
+    }
+
+    // redirect to Login page
+    navigate("login");
+  };
+  return (
+    <div>
+      <form>
+        <label htmlFor="">First Name :</label>
+        <input
+          type="text"
+          name="fname"
+          value={user.fname}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <label htmlFor="">Last Name :</label>
+        <input
+          type="text"
+          name="lname"
+          value={user.lname}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <label htmlFor="">Email :</label>
+        <input
+          type="text"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <label htmlFor="">Password :</label>
+        <input
+          type="text"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <label htmlFor="">Confirm Password :</label>
+        <input
+          type="text"
+          name="confirmPassword"
+          value={user.confirmPassword}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <button type="button" onClick={handleSubmit}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;

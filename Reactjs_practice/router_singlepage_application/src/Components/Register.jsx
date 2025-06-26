79ab6717562fd1bf5a8ef45334bad53ec200a0ea
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-06-26 21:39:17 +0530

    components

--- Reactjs_practice/router_singlepage_application/src/Components/Register.jsx
+++ Reactjs_practice/router_singlepage_application/src/Components/Register.jsx
@@ -11,7 +11,6 @@ const Register = () => {
     password: "",
     confirmPassword: "",
   });
-  
   const [users, setUsers] = useState([]);
   const handleChange = (e) => {
     const newUser = { ...user };

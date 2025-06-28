44087d96c8defe811473623f2f0ab7334cfc27de
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-06-28 21:18:47 +0530

    components

--- Reactjs_practice/app1_render_props/src/Render_props/CommonCounter.jsx
+++ Reactjs_practice/app1_render_props/src/Render_props/CommonCounter.jsx
@@ -17,6 +17,7 @@ export default class CommonCounter extends Component {
               handleDecrement={handleDecrement}
               handleReset={handleReset}
             />
+            
           )}
         />
         

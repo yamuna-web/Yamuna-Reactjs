ccd12da04020efca6aebfc8bc60a60c458382ad3
yamuna-web <gundlaguttayamuna61@gmail.com>
2025-07-15 22:05:43 +0530

    components

--- Reactjs_practice/project-3/src/TodoApp.jsx
+++ Reactjs_practice/project-3/src/TodoApp.jsx
@@ -20,7 +20,6 @@ const TodoApp = () => {
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
       )
     );
-    
   };
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));

3c5f0a90c857fc81270df0e3eb586489e21a5c23
yamuna-web <gundlaguttayamuna61@gmail.com>
2026-04-01 21:22:32 +0530

    revision

--- React-Projects/hotel-reservation/src/App.jsx
+++ React-Projects/hotel-reservation/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   const [editData, setEditData] = useState(null);
 
   return (
+    
     <Provider store={store}>
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">🏨 Hotel Reservation System</h1>
